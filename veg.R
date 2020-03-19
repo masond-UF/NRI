292dce5422ce18cf112efbdb9636eb44afc578eb
David Mason <masond@ufl.edu>
2020-03-19 10:20:24 -0400

    for march
    isolated data scores from NMDS and used cbind to add these to the environmental information

--- veg.R
+++ veg.R
@@ -3,6 +3,7 @@ library(lubridate)
 library(devtools)
 library(ggord)
 library(vegan)
+library(tidyverse)
 # bring in all of the data
 veg_all <- read.csv("OKMME_veg.csv") 
 # convert the date using lubridate
@@ -17,13 +18,22 @@ june_all <- veg_all[which(veg_all$Month==6),]
 july_all <- veg_all[which(veg_all$Month==7),]
 # subset the data to get just vegetation
 march_veg <- march_all[,9:97]
+march_env <- march_all[,1:8]
 april_veg <- april_all[,9:97]
 may_veg <- may_all[,9:97]
 june_veg <- june_all[,9:97]
 july_veg <- july_all[,9:97]
 # Create ordinations ####
 march_ord <- metaMDS(march_veg)
-ggord(march_ord, grp_in = march_all$Treatment, ellipse = FALSE, arrow = NULL,
+# Using the scores function from vegan to extract 
+# the site scores and convert to a data.frame
+march_data_scores <- as.data.frame(scores(march_ord))  
+# create a column of site names, from the rownames of data.scores
+march_data_scores_env <- cbind(march_env, march_data_scores)
+
+
+
+ggord(march_ord, grp_in = c(march_all$Carrion, march_all$Exclusion), ellipse = FALSE, arrow = NULL,
 			alpha = 0.8, var_sub = FALSE)
 
 
