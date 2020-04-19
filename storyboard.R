2253a011de9ccd52caa498f8cd9876c32c4ddb34
David Mason <masond@ufl.edu>
2020-04-18 23:54:12 -0400

    readme & metadata

--- storyboard.R
+++ storyboard.R
@@ -1,11 +1,4 @@
-# Making the storyboard
-
-# run concept_diagram.R
-# run seed_GLM.R
-# run spec_time.R
-# run veg.R
- # run concept_diagram.R
-
+# Saving the images for the storyboard
 ggsave(plot = diagram, "diagram.png", width = 15, height = 10, 
 			 units = "in", dpi = 300)
 ggsave(plot = bank, "seeds.png", width = 15, height = 10, 
